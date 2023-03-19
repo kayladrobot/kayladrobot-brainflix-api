4aa66364a46a19915c2bd2a4f9a0810789859831
“Kayla <kayladrobot@gmail.com>
2023-03-19 16:32:59 -0700

    set up packages and app.listen

---
+++ routes/videoRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router()
+
+// router.get('/videos', (req, res) => {
+//     console.log("request received")
+// })
+
+module.exports = router;
