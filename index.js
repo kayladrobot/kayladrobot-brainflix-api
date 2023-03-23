e74a0ab3deb13b259c3a9dd38b6a08a773c5060b
“Kayla <kayladrobot@gmail.com>
2023-03-22 20:48:19 -0700

    add cors and middleware

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const app = express();
 const PORT = 8080;
 const videoRoutes = require("./routes/videoRoutes")
 
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    if (req.method === 'POST' && req.headers['content-type'] !== 'application/json') {
+        res.status(400).send('Server require application/json');
+    } else {
+        next();
+    }
+})
+
+app.use(express.static('public'))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 app.use("/videos", videoRoutes)
 
 app.listen(PORT, () => {
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const fs = require('fs');
+const { title } = require("process");
+const uniqid = require('uniqid');
 
 router.get('/', (req, res) => {
   fs.readFile('./data/videos.json', (err, data) => {
@@ -18,5 +20,50 @@ router.get('/', (req, res) => {
     }
   });
 });
+
+router.get('/:id', (req, res) => {
+  const videoId = req.params.id;
+  fs.readFile('./data/videos.json', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      const videos = JSON.parse(data);
+      const video = videos.find(v => v.id === videoId);
+      if (video) {
+        res.send(video);
+      } else {
+        res.status(404).send('Video not found');
+      }
+    }
+  });
+});
+
+router.post('/', (req, res) => {
+  fs.readFile('./data/videos.json', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      const videos = JSON.parse(data);
+      const newVideo = {
+        id: uniqid(),
+        title: req.body.title,
+        channel: req.body.channel,
+        image: 'path/to/image'
+      };
+      videos.push(newVideo);
+      fs.writeFile('./data/videos.json', JSON.stringify(videos), (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.status(201).send(newVideo);
+        }
+      });
+    }
+  });
+});
+
 
 module.exports = router;
